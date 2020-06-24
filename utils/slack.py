2bb4dd6ea4bf142758bf2d6879637598be5ba94b
Victor GRENU <victor.grenu@gmail.com>
2020-06-24 08:42:23 +0200

    fix slack message for mobile app (readability)

--- utils/slack.py
+++ utils/slack.py
@@ -7,13 +7,13 @@ def speak_slack(SlackWebHook, alias, account, spend, running_ec2, running_rds, r
         
         slack.post(text="""
             AWS Account: `""" + str(account) + """` - `""" + str(alias) + """`\n
-            :money_with_wings: Current Spend MTD (`USD`): `""" + str(spend[0]) + """`\n
-            :money_with_wings: Forecasted Monthly Spend (USD):  `""" + str(spend[1]) + """`\n
-            :arrow_right: Total number of running EC2 instance(s): """ + str(len(running_ec2)) + """\n
-            :arrow_right: Total number of running RDS instance(s): """ + str(len(running_rds)) + """\n
-            :arrow_right: Total number of running Glue Dev Endpoint(s): """ + str(len(running_glue)) + """\n
-            :arrow_right: Total number of running SageMaker Notebook instance(s): """ + str(len(running_sage)) + """\n
-            :arrow_right: Total number of running Redshift Cluster(s): """ + str(len(running_redshift)) + """\n""")
+            :money_with_wings: Current MTD Spend (`USD`): `""" + str(spend[0]) + """`\n
+            :money_with_wings: Forecasted Monthly Spend (`USD`):  `""" + str(spend[1]) + """`\n
+            :arrow_right: EC2 instance(s): """ + str(len(running_ec2)) + """\n
+            :arrow_right: RDS instance(s): """ + str(len(running_rds)) + """\n
+            :arrow_right: Glue Dev Endpoint(s): """ + str(len(running_glue)) + """\n
+            :arrow_right: SageMaker Notebook instance(s): """ + str(len(running_sage)) + """\n
+            :arrow_right: Redshift Cluster(s): """ + str(len(running_redshift)) + """\n""")
         if len(running_ec2) > 0:
             slack.post(text="""""".join([f"\n • EC2: `{r['ec2_id']}`  `{r['ec2_type']}`  `{r['ec2_state']}`  `{r['region']}`  `{r['ec2_launch_time']}`  `{r['ec2_name']}`" for r in running_ec2]) + """""")
         if len(running_rds) > 0:
